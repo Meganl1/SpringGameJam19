3062570117a67a83ba61607ce905251237c563bf
Bt2solomon <44513630+Bt2solomon@users.noreply.github.com>
2019-01-25 23:04:16 -0600

    Add files via upload

---
+++ EnemyChase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour {
+
+	public float speed;
+	public float stoppingDistance;
+
+	private Transform target;
+	
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Player").
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
