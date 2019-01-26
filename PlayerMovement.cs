c8ec8399f5980b772f72c303b8ffddc2920cdf5b
Meganl1 <44512751+Meganl1@users.noreply.github.com>
2019-01-25 23:07:16 -0600

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	public float moveTime;
+	int check = 0;
+
+	float horizontal;
+	float vertical;
+
+	Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		horizontal = Input.GetAxisRaw("Horizontal");
+		vertical = Input.GetAxisRaw("Vertical");
+
+	}
+
+	private void FixedUpdate()
+	{
+
+				if(Input.GetKeyDown(KeyCode.LeftShift))
+				{
+					speed +=100;
+					check = 1;
+				}
+
+				rb.velocity = new Vector2(horizontal * speed * Time.deltaTime, vertical * speed * Time.deltaTime);
+
+				if(check == 1)
+				{
+					speed -=100;
+					check = 0;
+				}
+	}
+}
