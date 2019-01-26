1569bca07a964364d8e812497a287f413882d084
Bt2solomon <44513630+Bt2solomon@users.noreply.github.com>
2019-01-26 17:26:59 -0600

    Add files via upload

---
+++ Homer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homer : MonoBehaviour {
+
+	public float speed;
+	public float stoppingDistance;
+
+	private Transform target;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Home").GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Vector2.Distance(transform.position, target.position) > stoppingDistance){
+			transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+		}
+	}
+}
---
+++ LightShrink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightShrink : MonoBehaviour {
+public float decreaseRate;
+Light lt;
+ float originalRange;
+ float Range;
+	// Use this for initialization
+	void Start () {
+		lt = GetComponent<Light>();
+    Range = lt.range;
+		originalRange = lt.range;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		lt.range -= decreaseRate/1000;
+	}
+
+  void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Home"))
+		{
+			lt.range = 15;
+      Range = originalRange;
+		}
+	}
+}
